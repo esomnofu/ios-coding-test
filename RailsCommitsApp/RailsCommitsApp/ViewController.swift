dbf8351d90051ed06cc16449f1de1f7c3db24e51
esomnofu <mresomnofu@gmail.com>
2019-12-27 10:58:10 +0100

    added new ios project

---
+++ RailsCommitsApp/RailsCommitsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RailsCommitsApp
+//
+//  Created by Malik on 27/12/2019.
+//  Copyright © 2019 MrOfu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
